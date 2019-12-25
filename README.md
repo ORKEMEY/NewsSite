6794a137946b9ffeec73f808c2c4a638f9ec5d94
ORKEMEY <p.artem.p.15@gmail.com>
2019-12-26 01:50:49 +0200

    Added adding of new news

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+[![Build Status](https://travis-ci.com/ORKEMEY/NewsSite.svg?branch=master)](https://travis-ci.com/ORKEMEY/NewsSite)
 # NewsSite
 Lab 6
--- src/server.js
+++ src/server.js
@@ -1,26 +1,114 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const server = require('http').createServer(app);
+const crypto = require('crypto');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const { MongoClient } = require('mongodb');
+const { ObjectID } = require('mongodb');
+
+const url = 'mongodb+srv://Someone:kpi123@news-e7tux.mongodb.net/news?retryWrites=true&w=majority';
 
 app.listen(3000, () => {
-    console.log('Server is up!');
+  console.log('Server is up!');
 });
 
 app.set('view engine', 'ejs');
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/public/`));
 
 const news = [
-    { header: 'Header 1', content: 'News 1' },
-    { header: 'Header 2', content: 'News 2' },
+  { header: 'Header 1', content: 'News 1' },
+  { header: 'Header 2', content: 'News 2' },
 ];
 
+app.post('/addNews', (request, response) => {
+  console.log('post');
+  const { Header } = request.body;
+  const { Content } = request.body;
+  if (Header.length !== 0 && Content.length !== 0) {
+    const mongoClient = new MongoClient(url, { useUnifiedTopology: true });
+
+    mongoClient.connect((err, client) => {
+      if (err) {
+        return console.log(err);
+      }
+      const db = client.db('news'); // подключение бд
+      const newsdb = db.collection('news'); // создание коллекции
+
+      const hash = crypto
+        .createHash('md5')
+        .update(Content)
+        .digest('hex');
+      console.log(hash);
+      newsdb.insertOne({ id: hash, header: Header, content: Content }, (err, result) => {
+        if (err) return console.log(err);
+        console.log(result);
+      });
+      newsdb.find().toArray((err, results) => {
+        if (err) {
+          return console.log(err);
+        }
+        response.render(`${__dirname}/views/index.ejs`, { results });
+      });
+      client.close();
+    });
+  }
+});
+
 app.use('/News/:id', (request, response) => {
-    const { id } = request.params;
-    console.log(id);
-    response.render(`${__dirname}/views/News.ejs`, { news: news[id] });
+  const { id } = request.params;
+  app.use(express.static(`${__dirname}/public`));
+
+  const mongoClient = new MongoClient(url, { useUnifiedTopology: true });
+
+  mongoClient.connect((err, client) => {
+    if (err) {
+      return console.log(err);
+    }
+    const db = client.db('news'); // подключение бд
+    const newsdb = db.collection('news'); // создание коллекции
+
+    console.log(`idSite: ${String(request.params.id)}`);
+    const a = String(id);
+    newsdb.findOne({ header: String(request.params.id) }, (err, result) => {
+      if (err) {
+        return console.log(err);
+      }
+
+      console.log(`idSite: ${String(id)} ${typeof id}`);
+      console.log(`idSite: ${result}`);
+      if (result === null) {
+        response.render(`${__dirname}/views/News.ejs`, {
+          header: 'a' /* result.header */,
+          content: 'a' /* result.content */,
+        });
+      }
+    });
+
+    client.close();
+  });
 });
 
 app.use('/', (request, response) => {
-    response.render(`${__dirname}/views/index.ejs`, { news });
+  const mongoClient = new MongoClient(url, { useUnifiedTopology: true });
+
+  mongoClient.connect((err, client) => {
+    if (err) {
+      return console.log(err);
+    }
+    const db = client.db('news'); // подключение бд
+    const newsdb = db.collection('news'); // создание коллекции
+
+    newsdb.find().toArray((err, results) => {
+      if (err) {
+        return console.log(err);
+      }
+
+      response.render(`${__dirname}/views/index.ejs`, { results });
+    });
+    client.close();
+  });
 });
