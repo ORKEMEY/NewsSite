0252fda361fcb1c87c0eee226c157fb0bda711aa
ORKEMEY <p.artem.p.15@gmail.com>
2019-12-26 21:20:50 +0200

    Fixed bugs, added config.json, use mongoose

--- src/server.js
+++ src/server.js
@@ -2,17 +2,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const server = require('http').createServer(app);
-const crypto = require('crypto');
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const { MongoClient } = require('mongodb');
-const { ObjectID } = require('mongodb');
+const mongoose = require('mongoose');
 
-const url = 'mongodb+srv://Someone:kpi123@news-e7tux.mongodb.net/news?retryWrites=true&w=majority';
+const CONFIG = require(`${__dirname}/../config.json`);
+const { URL } = CONFIG;
+const PORT = process.env.PORT || CONFIG.PORT;
 
-const PORT = process.env.PORT || 3000;
+const { Schema } = mongoose;
+
+const newsScheme = new Schema({
+  header: String,
+  content: String,
+});
+
+mongoose.connect(URL, { useUnifiedTopology: true });
+const News = mongoose.model('News', newsScheme);
+
 app.listen(PORT, () => {
   console.log('Server is up!');
 });
@@ -20,95 +27,48 @@ app.listen(PORT, () => {
 app.set('view engine', 'ejs');
 app.use(express.static(`${__dirname}/public/`));
 
-const news = [
-  { header: 'Header 1', content: 'News 1' },
-  { header: 'Header 2', content: 'News 2' },
-];
-
 app.post('/addNews', (request, response) => {
   console.log('post');
   const { Header } = request.body;
   const { Content } = request.body;
   if (Header.length !== 0 && Content.length !== 0) {
-    const mongoClient = new MongoClient(url, { useUnifiedTopology: true });
+    const news = new News({
+      header: Header,
+      content: Content,
+    });
 
-    mongoClient.connect((err, client) => {
-      if (err) {
-        return console.log(err);
-      }
-      const db = client.db('news'); // подключение бд
-      const newsdb = db.collection('news'); // создание коллекции
+    news.save(err => {
+      if (err) return console.log(err);
+      console.log('New object: ', news);
 
-      const hash = crypto
-        .createHash('md5')
-        .update(Content)
-        .digest('hex');
-      console.log(hash);
-      newsdb.insertOne({ id: hash, header: Header, content: Content }, (err, result) => {
+      News.find({}, (err, news) => {
         if (err) return console.log(err);
-        console.log(result);
+        response.render(`${__dirname}/views/index.ejs`, { news });
       });
-      newsdb.find().toArray((err, results) => {
-        if (err) {
-          return console.log(err);
-        }
-        response.render(`${__dirname}/views/index.ejs`, { results });
-      });
-      client.close();
     });
   }
 });
 
 app.use('/News/:id', (request, response) => {
-  const { id } = request.params;
+  let { id } = request.params;
   app.use(express.static(`${__dirname}/public`));
 
-  const mongoClient = new MongoClient(url, { useUnifiedTopology: true });
+  id = id.slice(1, id.length);
 
-  mongoClient.connect((err, client) => {
-    if (err) {
-      return console.log(err);
+  News.findOne({ _id: id }, (err, news) => {
+    if (err) return console.log(err);
+
+    if (news !== null) {
+      response.render(`${__dirname}/views/News.ejs`, { news });
+    } else {
+      response.sendStatus(404);
     }
-    const db = client.db('news'); // подключение бд
-    const newsdb = db.collection('news'); // создание коллекции
-
-    console.log(`idSite: ${String(request.params.id)}`);
-    const a = String(id);
-    newsdb.findOne({ header: String(request.params.id) }, (err, result) => {
-      if (err) { return console.log(err);}
-
-      console.log(`idSite: ${String(id)} ${typeof id}`);
-      console.log(`idSite: ${result}`);
-
-      if (result === null) {
-        response.render(`${__dirname}/views/News.ejs`, {
-          header: 'a' /* result.header */,
-          content: 'a' /* result.content */,
-        });
-      }
-    });
-
-    client.close();
   });
 });
 
 app.use('/', (request, response) => {
-  const mongoClient = new MongoClient(url, { useUnifiedTopology: true });
-
-  mongoClient.connect((err, client) => {
-    if (err) {
-      return console.log(err);
-    }
-    const db = client.db('news'); // подключение бд
-    const newsdb = db.collection('news'); // создание коллекции
-
-    newsdb.find().toArray((err, results) => {
-      if (err) {
-        return console.log(err);
-      }
-
-      response.render(`${__dirname}/views/index.ejs`, { results });
-    });
-    client.close();
+  News.find({}, (err, news) => {
+    if (err) return console.log(err);
+    response.render(`${__dirname}/views/index.ejs`, { news });
   });
 });
